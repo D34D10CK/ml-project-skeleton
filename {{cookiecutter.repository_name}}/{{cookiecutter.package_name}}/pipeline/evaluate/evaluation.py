1f6fcd1736db7b20ca81a12cdc70fecc043e745e
Guillaume Mollard <gm@visium.ch>
2022-08-05 15:14:56 +0200

    fix lint issues

--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/pipeline/evaluate/evaluation.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/pipeline/evaluate/evaluation.py
@@ -1,5 +1,10 @@
-def main():
+"""Module for model evalutation."""
+
+
+def main() -> None:
+    """Main function, print hello message."""
     print("hello from evaluation")
+
 
 if __name__ == "__main__":
     main()
